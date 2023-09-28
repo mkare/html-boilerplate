15376d145a4eb16a020747c8112f56151ad2a42e
Mustafa Morbel <mustafamorbel@gmail.com>
2023-09-28 05:09:52 +0300

    intersect header animation on scroll

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,3 +1,14 @@
 export default function Header() {
-  console.log("Header component");
+  const header = document.getElementById("header");
+  const intercept = document.createElement("div");
+  const content = document.querySelector("#siteMain");
+
+  intercept.setAttribute("data-observer-intercept", "");
+  content.before(intercept);
+
+  const observer = new IntersectionObserver(([entry]) => {
+    header.classList.toggle("active", !entry.isIntersecting);
+  });
+
+  observer.observe(intercept);
 }
